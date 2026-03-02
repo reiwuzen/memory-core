c416721d6a3c2d5382c2ddf621390dd7133b4675
Rei WuZen <reiwuzen@gmail.com>
2026-03-02 20:33:57 +0530

    refactor(editor): remove legacy editor implementation after adopting blocky packages

--- src/types/result.ts
+++
@@ -1,6 +0,0 @@
-export type Result<T = never, E = string> =
-  [T] extends [never]
-    ? { ok: true }
-      | { ok: false; error: E }
-    : { ok: true; value: T }
-      | { ok: false; error: E };
