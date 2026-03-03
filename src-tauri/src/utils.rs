345101ffb0e2b122d52fae3891abda68bdf14a1f
Rei WuZen <reiwuzen@gmail.com>
2026-03-03 22:25:32 +0530

    refactor(rs/utils): borrow AppHandle instead of taking ownership

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use tauri::{AppHandle, Manager, command};
 
 #[command]
-pub fn get_app_dir(app: AppHandle) -> Result<PathBuf, String> {
+pub fn get_app_dir(app: &AppHandle) -> Result<PathBuf, String> {
     let app_dir = app.path().app_data_dir().map_err(|e| format!("Failed to get app data dir: {}", e)).unwrap();
     Ok(app_dir)
 }
