fcba1abae5e62935803240fcf54bbc2d8ce39858
Rei WuZen <reiwuzen@gmail.com>
2026-03-04 14:36:23 +0530

    refactor(models): unify page versioning model
    - remove legacy MemoryItem/MemoryNode system
    - make PageMeta.head_snapshot_id optional
    - VersionedPage now contains only snapshots
    - simplify serialization structures

--- src-tauri/src/schema.rs
+++ src-tauri/src/schema.rs
@@ -6,7 +6,7 @@ pub struct PageMeta {
     pub id: String,
     #[serde(rename = "type")]
     pub page_type: PageType,
-    pub head_snapshot_id: String,
+    pub head_snapshot_id: Option<String>,
     pub title: String,
     pub created_at: DateTime<Utc>,
     pub tags: Vec<String>,
@@ -37,11 +37,10 @@ pub struct Snapshot {
     pub comment: Option<String>,
 }
 
-
 #[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Book {
-    pub    id: String,
+    pub id: String,
     pub title: String,
     pub created_at: String,
     #[serde(rename = "type")]
@@ -52,42 +51,7 @@ pub struct Book {
 #[serde(rename_all = "camelCase")]
 pub struct VersionedPage {
     pub page_meta: PageMeta,
-    pub head_snapshot: Snapshot,
-    pub snapshots: Vec<Snapshot>
-}
-
-
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-pub struct MemoryItem {
-    pub memory_id: String,
-    pub created_at: DateTime<Utc>,
-    pub head_node_id: String,
-    pub title: String,
-    pub memory_type: MemoryType,
-
-    pub last_updated_at: String,
-    pub last_opened_at: String,
-    pub tags: Vec<Tag>,
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
-pub enum MemoryType {
-    Diary,
-    Fact,
-    Event,
-    Schedule,
-    Generic,
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
-pub struct MemoryNode {
-    pub node_id: String,
-    pub memory_id: String,
-    pub parent_node_id: Option<String>,
-    pub created_at: DateTime<Utc>,
-    // pub content_string: String,
-    pub content_json: serde_json::Value,
-    pub change_reason: Option<String>,
+    pub snapshots: Vec<Snapshot>,
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
@@ -96,11 +60,4 @@ pub struct Tag {
     pub label: String,
     pub description: String,
     pub priority: f32,
-}
-
-#[derive(serde::Serialize)]
-pub struct MemoryPayload {
-    pub memory_item: MemoryItem,
-    pub head_node: MemoryNode,
-    pub nodes: Vec<MemoryNode>,
 }
